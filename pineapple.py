07e044af8e7c1907f7dad6bae75e0c03144464da
Doğan Can Eren <dgncnrn@gmail.com>
2019-12-20 16:58:04 +0300

    mainpage, profilepage

---
+++ pineapple.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, abort, make_response, request, send_file, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('MainPage.html')
+
+@app.route('/profile/')
+def profile():
+    return render_template('ProfilePage.html')
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
